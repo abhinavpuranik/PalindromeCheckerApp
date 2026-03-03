3a83ed27769c8102a3297084cfc1a279e36a54eb
abhinavpuranik <abhunav.puranik@gmail.com>
2026-03-03 15:57:11 +0530

    Coded Use Case One to Display a Welcome Message

---
+++ src/palindromeCheckerApp/UseCaseOnePalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+package palindromeCheckerApp;
+
+public class UseCaseOnePalindromeCheckerApp {
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to the Palindrome Checker Management System");
+			System.out.println("Version 1.0");
+			System.out.println("System initialized successfully");
+			
+		}
+}
