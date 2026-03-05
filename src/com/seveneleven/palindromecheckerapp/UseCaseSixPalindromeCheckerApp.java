aa1e3ca410f83c0ec609d6f9e7622bc2b354daa9
abhinavpuranik <abhunav.puranik@gmail.com>
2026-03-05 10:34:06 +0530

    Coded UC6 to solve IsPalindrome using FIFO LIFO output comparison

---
+++ src/com/seveneleven/palindromecheckerapp/UseCaseSixPalindromeCheckerApp.java
@@ -0,0 +1,45 @@
+/*
+ * @author developer
+ * @version 6.0
+ * Palindrome validation using output comparison stack vs queue
+ */
+
+package com.seveneleven.palindromecheckerapp;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class UseCaseSixPalindromeCheckerApp {
+	
+	public static void main(String[] args) {
+		String input = "civic";
+		
+		
+		Queue<Character>queue = new LinkedList<>();
+		
+		Stack<Character> stack = new Stack<>();
+		
+		
+		for(char c : input.toCharArray()) {
+			queue.offer(c);
+			stack.push(c);
+			
+		}
+		
+		boolean isPalindrome = true;
+		
+		while(!queue.isEmpty()) {
+			if(queue.poll() != stack.pop()) {
+				isPalindrome = false;
+				break;
+			}
+		}
+		
+		System.out.println("Input: " + input);
+		System.out.println("Is Palindrome? " + isPalindrome);
+		
+		
+	}
+
+}
